443c92914b62e94801eaf67c4a9eb3208be69279
Rakeshmaity001 <rakesh.maity80@gmail.com>
2023-03-10 23:55:04 +0530

    added 3 new ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,13 @@
 
 
 print('adding all ml related code is here')
+
+def model1():
+    print('adding code for model1')
+
+def model2():
+    print('adding code for model2')
+
+def model3():
+    print('adding code for model3')
+
